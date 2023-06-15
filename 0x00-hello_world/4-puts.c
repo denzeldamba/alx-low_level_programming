48dc5ef627b8c054d6297a2a93cdf7d22146f368
denzeldamba <denzeldamba42@gmail.com>
2023-06-15 10:54:23 -0700

    Name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a specific statement using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
