ac79bbcf177f5a1e7d532e0006b2d052dc9f6433
denzeldamba <denzeldamba42@gmail.com>
2023-06-16 13:16:00 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : Prints all the letters in small case from a-z
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
