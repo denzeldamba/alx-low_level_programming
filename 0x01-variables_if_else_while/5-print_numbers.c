e059fe6bffadbdcdeb845bca8fff3eb9e1208d10
denzeldamba <denzeldamba42@gmail.com>
2023-06-16 13:31:04 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,10 +11,12 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num = 9; num++)
+	for (num = 0; num < 9; num++)
 	{
-		printf("%d\n", num);
+		printf("%d\", num);
 	}
+
+	printf("\n");
 
 	return (0);
 }
