d931a093d79c0956b4ac67a05b63764cee29111a
denzeldamba <denzeldamba42@gmail.com>
2023-06-16 13:34:08 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,13 +5,13 @@
  *
  * Description : Prints all single digits in decimal system
  *
- * return : Always 0 (Success)
+ * Return : Always 0 (Success)
  */
 int main(void)
 {
 	int num;
 
-	for (num = 0; num < 9; num++)
+	for (num = 0; num < 10; num++)
 	{
 		printf("%d", num);
 	}
