b412ec80bfa1c6530a78d5d7d27701ea61bd9ca2
denzeldamba <denzeldamba42@gmail.com>
2023-06-17 05:58:07 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /**
- * main- Entry point
- *
- * Description- prints all the hexadecimal
+ * main- prints all the hexadecimal
  *
  * Return-0 (Success)
  */
@@ -11,9 +9,9 @@ int main(void)
 {
 	char digit;
 
-	for( digit = '0'; digit <= '9'; digit++)
+	for (digit = '0'; digit <= '9'; digit++)
 		putchar(digit);
-	for( digit = 'a'; digit <= 'f'; digit++)
+	for (digit = 'a'; digit <= 'f'; digit++)
 		putchar(digit);
 
 	putchar('\n');
