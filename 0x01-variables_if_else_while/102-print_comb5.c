05e74cc344027b95dbe6e652a5adeeffb9ebff42
denzeldamba <denzeldamba42@gmail.com>
2023-06-17 14:46:38 -0700

    Software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,7 @@
  *
  * Description: Prints all possible combinations of two two-digit numbers
  * Numbers range from 0 to 99 and are printed with two digits
- * Numbers are separated by a space and combinations are separated by comma and space
+ * Numbers  separated by space and combinations separated by comma and space
  * Combinations are printed in ascending order
  *
  * Return: Always 0 (Success)
@@ -16,7 +16,7 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = i; j <= 99; j++)
+		for (j = 1; j <= 99; j++)
 		{
 		putchar('0' + (i / 10));
 		putchar('0' + (i % 10));
@@ -24,7 +24,7 @@ int main(void)
 		putchar(' ');
 
 		putchar('0' + (j / 10));
- 		putchar('0' + (j % 10));
+		putchar('0' + (j % 10));
 
 			if (i != 99 || j != 99)
 			{
