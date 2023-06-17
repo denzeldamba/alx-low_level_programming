46da186ca5e9c3a535c447646d90d846a4d3e150
denzeldamba <denzeldamba42@gmail.com>
2023-06-17 06:33:44 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-printt_comb.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of single-digit numbers.
+ *              Numbers are separated by a comma and space.
+ *              Numbers are printed in ascending order.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	putchar('0');
+	putchar(',');
+	putchar(' ');
+
+	putchar('1');
+	putchar(',');
+	putchar(' ');
+
+	putchar('2');
+	putchar(',');
+	putchar(' ');
+
+	putchar('3');
+	putchar(',');
+	putchar(' ');
+
+	putchar('4');
+	putchar(',');
+	putchar(' ');
+
+	putchar('5');
+	putchar(',');
+	putchar(' ');
+
+	putchar('6');
+	putchar(',');
+	putchar(' ');
+
+	putchar('7');
+	putchar(',');
+	putchar(' ');
+
+	putchar('8');
+	putchar(',');
+	putchar(' ');
+
+	putchar('9');
+	putchar('\n');
+
+	return (0);
+}
+
