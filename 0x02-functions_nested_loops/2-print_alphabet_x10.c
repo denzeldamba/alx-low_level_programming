9a2c7152113ee0482dc95852a8111f8e8c05c545
denzeldamba <denzeldamba42@gmail.com>
2023-06-21 13:51:04 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints letters 10 times in lower case from a-z
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i<=9; i++)
+	{
+		while ( letter = 'a')
+		{
+			_putchar(letter);
+			letter++
+		}
+
+		_putchar('\n')
+}
