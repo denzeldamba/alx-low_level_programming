4e61259ffae6071cb93d9daf80b4b1a8602a8024
denzeldamba <denzeldamba42@gmail.com>
2023-06-21 14:09:30 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,11 @@ void print_alphabet_x10(void)
 	char letter;
 	int i;
 
-	for (i = 0; i<=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		letter = 'a';
 
-		while (letter  <=  'z')
+		while (letter <= 'z')
 		{
 			_putchar(letter);
 			letter++;
