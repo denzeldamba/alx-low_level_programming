354336519bf38646f7016db24f0f813f54babdc8
denzeldamba <denzeldamba42@gmail.com>
2023-06-21 20:33:45 -0700

    Islower

---
+++ 0x02-functions_nested_loops/3-lower.c
@@ -0,0 +1,481 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}#include "main.h"
+
+/**
+ * _islower - Checks if a character is a lowercase letter 
+ * @c : The character to be checked
+ *
+ * Return: @ if c is a lowercase letter, 0 otherwise
+ */
+
+int -islower(int c)
+{
+	If (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
