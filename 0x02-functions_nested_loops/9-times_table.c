5c21bcf4449d5fa58ccc9106e20e86606a3f3537
denzeldamba <denzeldamba42@gmail.com>
2023-06-21 22:31:47 -0700

    . Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,11 +20,18 @@ void times_table(void)
 			}
 
 			if (result < 10)
+			{
 				_putchar(' ');
-			else
-				_putchar((result / 10) + '0');
+				_putchar(' ');
+				_putchar((result % 10) + '0');
+			}	
 
-			_putchar((result % 10) + '0');
+			else
+			{
+				_putchar(' ')	
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
