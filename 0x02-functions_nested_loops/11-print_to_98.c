58044d3bd95d6ae16154257f54caf329070df159
denzeldamba <denzeldamba42@gmail.com>
2023-06-26 23:39:57 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,11 +11,7 @@ void print_to_98(int n)
 {
 	while (n != 98)
 	{
-		if (n > 99)
-		{
-			_putchar(n / 100 % 10 + '0');
-		}
-
+		_putchar(n / 100 % 10 + '0');
 		_putchar(n / 10 % 10 + '0');
 		_putchar(n % 10 + '0');
 		_putchar(',');
