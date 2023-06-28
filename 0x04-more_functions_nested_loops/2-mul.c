54d5fa3f21c6d9ab2f074d823c3233fa329faab7
denzeldamba <denzeldamba42@gmail.com>
2023-06-27 23:43:15 -0700

    Collaboratio is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two intergers
+ * @a: THe first interger
+ * @b: The second interger 
+ *
+ * Return: The product of a and b
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
