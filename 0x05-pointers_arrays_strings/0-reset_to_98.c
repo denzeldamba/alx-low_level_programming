978e45dd90625c5ffcf0b251742420b24e361188
denzeldamba <denzeldamba42@gmail.com>
2023-06-29 02:54:18 -0700

    Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,25 +7,4 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-	value = *n;
-
-	if (value == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	while (value != 0)
-	{
-		int remainder = value % 16;
-		char hexChar;
-
-		if (remainder < 10)
-			hexChar = remainder + '0';
-		else
-			hexChar = remainder - 10 + 'a';
-
-		_putchar(hexChar);
-		value /= 16;
-	}
 }
