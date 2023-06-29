dc0e6b3a998a9fade4708ed257caa279ca7ada48
denzeldamba <denzeldamba42@gmail.com>
2023-06-29 02:45:57 -0700

    Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * reset_to_98- Updates the value pointed to by n to 98
+ * @n: Pointer to the integer to be updates.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	unsigned long int value = (unsigned long int)n;
+
+	if (value == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (value != 0)
+	{
+		int remainder = value % 16;
+		char hexChar;
+
+		if (remainder < 10)
+			hexChar = remainder + '0';
+		else
+			hexChar = remainder - 10 + 'a';
+
+		_putchar(hexChar);
+		value /= 16;
+	}
+}
