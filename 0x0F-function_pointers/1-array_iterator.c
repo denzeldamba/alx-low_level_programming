d2c4c56b6a30fd5b1ecd1ebeb1c9cc27be8ba9f0
denzeldamba <denzeldamba42@gmail.com>
2023-07-19 21:39:02 +0300

    . If you spend too much time thinking about a thing, you'll never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - Executes a function on each element of an array.
+ * @array: The array to iterate through
+ * @size: Size of the array
+ * @action: Function pointer to execute
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
